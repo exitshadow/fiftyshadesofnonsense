7a71156801f2764a3573d75269cbb6e1a2e70d36
exitshadow <exertingshadows@gmail.com>
2022-06-14 19:08:43 +0200

    yehee drawn a few circles

---
+++ circles.glsl
@@ -0,0 +1,51 @@
+// Author @patriciogv - 2015
+// http://patriciogonzalezvivo.com
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+/*Basically we are using a re-interpretation of the space (based on the distance to the center) to make shapes. This technique is known as a “distance field” and is used in different ways from font outlines to 3D graphics.
+
+Try the following exercises:
+
+    Use step() to turn everything above 0.5 to white and everything below to 0.0.
+
+    Inverse the colors of the background and foreground.
+
+    Using smoothstep(), experiment with different values to get nice smooth borders on your circle.
+
+    Once you are happy with an implementation, make a function of it that you can reuse in the future.
+
+    Add color to the circle.
+
+    Can you animate your circle to grow and shrink, simulating a beating heart? (You can get some inspiration from the animation in the previous chapter.)
+
+    What about moving this circle? Can you move it and place different circles in a single billboard?
+    What happens if you combine distances fields together using different functions and operations?
+*/
+
+float drawCircle(vec2 uv, vec2 pos, float radius, float thickness){
+    float dist = distance(uv, pos);
+
+    float circle = step(radius, dist);
+    float innerCircle = step(radius + thickness, 1.-dist);
+
+    circle += innerCircle;
+
+    return circle ;
+}
+
+void main(){
+	vec2 st = gl_FragCoord.xy/u_resolution;
+    st.y *= 1.25;
+    float pct = drawCircle(st, vec2( .3, .5 ), .5, .06);
+
+    vec3 color = vec3(pct);
+
+	gl_FragColor = vec4( color, 1.0 );
+}
